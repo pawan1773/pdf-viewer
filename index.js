a9fbb04a958b9b8c03641586bdddb5572d619897
pawan1773 <35738777+pawan1773@users.noreply.github.com>
2020-07-15 11:07:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 /*
 Copyright 2019 Adobe
 All Rights Reserved.
@@ -10,53 +12,83 @@ written permission of Adobe.
 
 /* Control the default view mode */
 const viewerConfig = {
-    /* Allowed possible values are "FIT_PAGE", "FIT_WIDTH" or "" */
-    defaultViewMode: "",
+	/* Allowed possible values are "FIT_PAGE", "FIT_WIDTH" or "" */
+	"defaultViewMode": "FIT_PAGE",
+	"embedMode": "FULL_WINDOW",
+	"showLeftHandPanel":false	
+};
+
+const profile = {
+	userProfile: {
+		name: 'topcoder',
+		firstName: 'topcoder',
+		lastName: 'topcoder',
+		email: 'topcoder@gmail.com',
+	}
 };
 
 /* Wait for Adobe Document Cloud View SDK to be ready */
 document.addEventListener("adobe_dc_view_sdk.ready", function () {
-    /* Initialize the AdobeDC View object */
-    var adobeDCView = new AdobeDC.View({
-        /* Pass your registered client id */
-        clientId: "a1648e3fdc99492d88bb309c70832c4c",
-        /* Pass the div id in which PDF should be rendered */
-        divId: "adobe-dc-view",
-    });
-    listenForFileUpload(adobeDCView);
-	adobeDCView.registerCallback(AdobeDC.View.Enum.CallbackType.EVENT_LISTENER, function(event) {
-     switch(event.type) {
-          case "DOCUMENT_OPEN":
-               ga('send', 'event', 'DOCUMENT_OPEN', event.data.fileName, 'open document');
-               break;
-          case 'PAGE_VIEW':
-               ga('send', 'event', 'PAGE_VIEW', `${event.data.pageNumber} of ${event.data.fileName}`, 'view page');
-               break;
-          case 'DOCUMENT_DOWNLOAD':
-               ga('send', 'event', 'DOCUMENT_DOWNLOAD', event.data.fileName, 'download document');
-               break;
-		 case 'TEXT_COPY':
-               ga('send', 'event', 'TEXT_COPY', `${event.data.copiedText} of ${event.data.fileName}`, 'text copy');
-               break;
-     } 
+	/* Initialize the AdobeDC View object */
+	var adobeDCView = new AdobeDC.View({
+		/* Pass your registered client id */
+		clientId: "a1648e3fdc99492d88bb309c70832c4c",
+		/* Pass the div id in which PDF should be rendered */
+		divId: "adobe-dc-view",
+	});
+	listenForFileUpload(adobeDCView);
+	adobeDCView.registerCallback(
+		AdobeDC.View.Enum.CallbackType.GET_USER_PROFILE_API,
+		function () {
+
+			return new Promise((resolve, reject) => {
+
+				resolve({
+					code: AdobeDC.View.Enum.ApiResponseCode.SUCCESS,
+					data: profile
+				})
+			})
+		});
+	adobeDCView.registerCallback(AdobeDC.View.Enum.CallbackType.EVENT_LISTENER, function (event) {
+		alert(event.type);
+		switch (event.type) {
+			case "DOCUMENT_OPEN":
+				alert('DOCUMENT_OPEN');
+				ga('send', 'event', 'DOCUMENT_OPEN', event.data.fileName, 'open document');
+				break;
+			case 'PAGE_VIEW':
+				ga('send', 'event', 'PAGE_VIEW', `${event.data.pageNumber} of ${event.data.fileName}`, 'view page');
+				break;
+			case 'DOCUMENT_DOWNLOAD':
+				ga('send', 'event', 'DOCUMENT_DOWNLOAD', event.data.fileName, 'download document');
+				break;
+			case 'TEXT_COPY':
+				ga('send', 'event', 'TEXT_COPY', `${event.data.copiedText} of ${event.data.fileName}`, 'text copy');
+				break;
+		}
 	});
 });
 
 function listenForFileUpload(adobeDCView) {
-  var fileToRead = document.getElementById("file-picker");
-  fileToRead.addEventListener("change", function(event) {
-     var files = fileToRead.files;
-     if (files.length > 0) {
-        var reader = new FileReader();
-        reader.onloadend = function(e) {
-            var filePromise = Promise.resolve(e.target.result);
-            // Pass the filePromise and name of the file to the previewFile API
-            adobeDCView.previewFile({
-                  content:{promise: filePromise},
-                  metaData:{ fileName: files[0].name }
-             }, {embedMode: "FULL_WINDOW"})
-        };
-        reader.readAsArrayBuffer(files[0]);
-      }
-    }, false);
+	var fileToRead = document.getElementById("file-picker");
+	fileToRead.addEventListener("change", function (event) {
+		var files = fileToRead.files;
+		if (files.length > 0) {
+			var reader = new FileReader();
+			reader.onloadend = function (e) {
+				var filePromise = Promise.resolve(e.target.result);
+				// Pass the filePromise and name of the file to the previewFile API
+				adobeDCView.previewFile({
+					content: {
+						promise: filePromise
+					},
+					metaData: {
+						fileName: files[0].name
+					}
+				}, viewerConfig)
+			};
+			reader.readAsArrayBuffer(files[0]);
+		}
+	}, false);
 }
+
